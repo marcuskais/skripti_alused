07e78edde788c3c9647c743e0a47b4cb46cae257
Marcus Kais <marcus.kais@khk.ee>
2020-11-18 15:11:34 +0200

    Praks3 kaust

---
+++ praks3/yl3.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# kusib kasutaja kaest tema ees- ja perekonnanime ja tervitab teda
+#
+#
+echo "Sisesta oma ees- ja perekonnanimi: "
+read eesnimi perekonnanimi
+echo "Tere tulemast, $eesnimi $perekonnanimi"
