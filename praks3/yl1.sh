429024c4ee2c03bb8e635c3a088be7d6a61f6915
Marcus Kais <marcus.kais@khk.ee>
2020-11-18 15:15:10 +0200

    Kusib nimesi ja tervitab kasutajat

---
+++ praks3/yl1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# kusib kasutaja kaest tema ees- ja perekonnanime ja tervitab teda
+#
+#
+#
+echo "Sisesta oma ees- ja perekonnanimi: "
+read eesnimi perekonnanimi
+echo "Tere tulemast, $eesnimi $perekonnanimi"
