37ba9767f53d580e04754fc7d6e6ea9cb1771dd7
Marcus Kais <marcus.kais@khk.ee>
2020-11-18 14:52:51 +0200

    Kuupäeva ja kellaajaga seotud informatsioon

---
+++ praks2/yl2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+#Valjastab kuupäevaga ja ajaga seotud info
+#
+kell=`date +%H:%M`
+kalender=`cal`
+kuupaev=`date +"%A, %B %d, %Y"`
+echo "Today is $kuupaev"
+echo
+echo "Time is $kell"
+echo
+echo "$kalender"
