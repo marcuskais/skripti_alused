7f1dc99f51e0bddacefcc6214d4e43b174d9f5e3
Marcus Kais <marcus.kais@khk.ee>
2020-11-30 19:31:09 +0200

    taiendus

---
+++ praks6/yl4.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+#
+#
+#algarvu kontroll
+#
+#tsukkel, genereerime esimesed 20 arvu
+for ((arv=1; arv<21; arv++ ))
+do
+#kontroll, et kas sisestatud arv on algarv voi ei ole.
+#
+#
+	if [ $arv -eq 0 -o $arv -eq 1 ]; then
+	echo "$arv ei ole algarv"
+	else
+		jagaja=2
+		jaak=$(($arv % $jagaja))
+		while [ $jaak -ne 0 ]
+		do
+		jagaja=$((jagaja + 1))
+		jaak=$(($arv % $jagaja))
+		done
+			if [ $arv -eq $jagaja ]; then
+				echo "$arv on algarv"
+			else
+				echo "$arv ei ole algarv"
+			fi
+fi
+done
