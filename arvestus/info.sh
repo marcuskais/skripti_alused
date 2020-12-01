963906780ed98b95d674ff2eb5da30bbc7d4aeae
Marcus Kais <marcus.kais@khk.ee>
2020-12-01 12:16:40 +0200

    lisasin moned kommentaarid juurde

--- arvestus/info.sh
+++ arvestus/info.sh
@@ -5,10 +5,14 @@
 #Skripti ülesanne: Tervitab sisselogitud kasutajat ja valjastab kuupaeva,
 #kellaajaga seotud informatsiooni.
 #
+#
+#
+#Kastuaja tervitus
 kastutajanimi=$(whoami)
 echo -n  "Tere, "
 echo -n $(whoami)
 echo "!"
 kell=`date +%H:%M`
 kuupaev=`date +"%d-%m-%Y"`
+#valjastame info
 echo "Tana on $kuupaev kell $kell"
